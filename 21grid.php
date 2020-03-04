dd470ec6be8180da3da4697718bb70ca97fbf44b
Rafid Adnan <princerafid01@gmail.com>
2020-03-04 17:38:01 +0600

    DCP-32: The Grid

---
+++ 21grid.php
@@ -0,0 +1,38 @@
+<?php
+$stdin = fopen('php://stdin', 'r');
+$stdout = fopen('php://stdout', 'w');
+fscanf(STDIN, "%d", $testcase); // reads number from STDIN
+$case = [];
+for ($i =0; $i<$testcase; $i++) {
+    fscanf(STDIN, "%d %d %d %d", $x1, $y1, $x2, $y2); // reads number from STDIN
+    $count =0;
+    if ($x1 > $x2) {
+        $big1 = $x1;
+        $small1 = $x2;
+    } else {
+        $big1 = $x2;
+        $small1 = $x1;
+    }
+    if ($y1 > $y2) {
+        $big2 = $y1;
+        $small2 = $y2;
+    } else {
+        $big2 = $y2;
+        $small2 = $y1;
+    }
+    for ($j=$small1; $j < $big1; $j++) {
+        $count++;
+    }
+    for ($j=$small2; $j < $big2; $j++) {
+        $count++;
+    }
+
+    $case[] = $count;
+}
+
+foreach ($case as $key => $value) {
+    fprintf(STDOUT, 'Case ');
+    fprintf(STDOUT, $key+1);
+    fprintf(STDOUT, ": ");
+    fprintf(STDOUT, $value.PHP_EOL);
+}
--- 4greatTheWork IsDone.php
+++ 4greatTheWork IsDone.php
@@ -1,11 +1,4 @@
 <?php
-// $stdin = fopen('php://stdin', 'r');
-// $stdout = fopen('php://stdout', 'w');
-
-// $line = fgets(STDIN); // reads one line from STDIN
-
-// fscanf(STDIN, "%d %d", $time_to_finish, $members); // reads times and memeber number from STDIN
-// fscanf(STDIN, "%d", $time_to_finish, $members); // reads number from STDIN
 $input = readline();
 $arr = explode(' ', $input);
 $finished_hour = $arr[0];
